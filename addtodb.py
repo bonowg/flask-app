39dd7ee93584f2a5289eff10878727154085d360
bonowwg <bonowg@gmail.com>
2018-10-05 18:24:47 +0200

    pagination

--- addtodb.py
+++ addtodb.py
@@ -2,17 +2,22 @@ import requests
 import random
 import json
 
-# for num in range(1,21):
-#     r = requests.post('http://127.0.0.1:5050/store/store-{0}'.format(num))
-#     print(r.status_code, r.reason)
 
+
+# for num in range(1,21):
+#     r = requests.post('http://127.0.0.1:5050/store/store-{0}'.format(num), headers=headers)
+#     print(r.status_code, r.reason)
+headers = {
+        "Authorization": "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1Mzg3NTY4MTcsImlhdCI6MTUzODc1NjUxNywibmJmIjoxNTM4NzU2NTE3LCJpZGVudGl0eSI6MX0.jNVOJkLBCQsv8U0mVUfXDf02e-ZNbgIiUdtmFIieQDM",
+        "Content-Type": "application/json"
+    }
 
 for num in range(1,51):
     price = random.uniform(1.1, 50.9)
-    store = random.randrange(172, 192)
+    store = random.randrange(1, 21)
     payload = { "price": price,
                 "store_id": store
                 }
-    headers = {'content-type': 'application/json'}
+
     r = requests.post('http://127.0.0.1:5050/item/item-{0}'.format(num), data=json.dumps(payload), headers=headers)
     print(r.status_code, r.reason)
